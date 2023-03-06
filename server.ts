046f20ea41f1c78e0e4d5439cdd602da7553418e
Todd Christensen <toddchristensen@protonmail.com>
2023-03-06 09:17:48 +0800

    random endpoint and converted responses to json

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { serve } from 'https://deno.land/std@0.178.0/http/server.ts';
+import { serve } from 'https://deno.land/std/http/server.ts';
 
 const messages = await Deno.readTextFile('messages.txt')
     .then((text) => text.trim().split('\n'));
@@ -8,9 +8,24 @@ function getRandomMessage() {
     return messages[randomIndex];
 }
 
-function requestHandler() {
-    const randomMessage = getRandomMessage();
-    return new Response(randomMessage);
+function requestHandler(request: Request) {
+    const { url } = request;
+    const { pathname } = new URL(url, `http://${request.headers.get("host")}`);
+
+    if (pathname === '/') {
+        const responseJson = { messages };
+        return new Response(JSON.stringify(responseJson), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } else if (pathname === '/random') {
+        const randomMessage = getRandomMessage();
+        const responseJson = { message: randomMessage };
+        return new Response(JSON.stringify(responseJson), {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } else {
+        return new Response('404 Not Found', { status: 404 });
+    }
 }
 
-serve(requestHandler);
+serve(requestHandler, { port: 8000 });
